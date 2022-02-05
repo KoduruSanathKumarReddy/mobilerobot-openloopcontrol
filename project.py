c6d19572aef64aeea5036d006d5af274cef15e16
KoduruSanathKumarReddy <koduru365@gmail.com>
2022-02-05 20:06:24 +0530

    updated images

--- project.py
+++ project.py
@@ -1,46 +1,4 @@
-# MobileRobot-Openloopcontrol
-## Aim:
-
-To develop a python control code to move the mobilerobot along the predefined path.
-
-## Equipments Required:
-1. RoboMaster EP core
-2. Python 3.7
-
-## Procedure
-
-Step1:
-
-<br/>
-
-Step2:
-
-<br/>
-
-Step3:
-
-<br/>
-
-Step4:
-
-<br/>
-
-Step5:
-
-<br/>
-
-## Program
-```python
 from robomaster import robot
-import time
-
-if __name__ == '__main__':
-    ep_robot = robot.Robot()
-    ep_robot.initialize(conn_type="ap")
-
-    ep_chassis = ep_robot.chassis
-
-    from robomaster import robot
 import time
 
 if __name__ == '__main__':
@@ -68,42 +26,3 @@ if __name__ == '__main__':
     ep_chassis.drive_speed(x=0,y=0,z=0)
     print("Completed...")
     ep_robot.close()
-
-
-
-    
-    ep_robot.close()
-```
-
-## MobileRobot Movement Image:
-
-![robo](robo1.png)
-![](robo2.png)
-Insert image here
-
-
-<br/>
-<br/>
-<br/>
-<br/>
-
-## MobileRobot Movement Video:
-https://youtu.be/TLKPUdjDF1o
-
-<br/>
-<br/>
-<br/>
-<br/>
-
-## Result:
-Thus the python program code is developed to move the mobilerobot in the predefined path.
-
-
-<br/>
-<br/>
-
-```
-Mobile Robotics Laboratory
-Department of Artificial Intelligence and Data Science/ Machine Learning
-Saveetha Engineering College
-```
